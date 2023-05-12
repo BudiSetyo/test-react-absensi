3936ce257edd4a4742858531857b54558f24f016
BudiSetyo <budisetyo176@gmail.com>
2023-05-12 19:27:03 +0700

    feat: initial commit

---
+++ src/pages/auth/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Auth = () => {
+  return (
+    <div>
+      <h1>This is Auth page</h1>
+    </div>
+  );
+};
+
+export default Auth;
---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+import Dashboard from "./dashboard";
+import Auth from "./auth";
+
+export { Dashboard, Auth };
