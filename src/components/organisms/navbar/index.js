396c23109de642c604755f9fc966599951314345
BudiSetyo <budisetyo176@gmail.com>
2023-05-18 01:37:15 +0700

    feat: add navbar check token

--- src/components/organisms/navbar/index.js
+++ src/components/organisms/navbar/index.js
@@ -1,10 +1,39 @@
 import React from "react";
 import { LogoutOutlined } from "@ant-design/icons";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
 import { logout } from "../../../redux/features/auth";
+// import { message } from "antd";
+import axios from "axios";
+const api = process.env.REACT_APP_API_URL;
 
 const Navbar = () => {
   const dispatch = useDispatch();
+  const authData = useSelector((state) => state.auth.value);
+
+  const checkToken = () => {
+    axios({
+      method: "get",
+      url: `${api}/auth`,
+      headers: {
+        Authorization: `Bearer ${authData.token}`,
+      },
+    })
+      .then((_) => {
+        return;
+      })
+      .catch((err) => {
+        if (err.response?.data.message === "You have to login!") {
+          return dispatch(logout());
+        }
+
+        return;
+      });
+  };
+
+  useEffect(() => {
+    checkToken();
+  }, []);
 
   return (
     <nav className="w-full bg-gray-600 py-2 px-8">
